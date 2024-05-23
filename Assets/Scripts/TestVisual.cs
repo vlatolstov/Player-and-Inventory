d2192a888c173355f685b6cc7528eedc443059cc
Владимир Толстов <vlatolstov@gmail.com>
2024-05-23 22:44:02 +0400

    добавлен тест

---
+++ Assets/Scripts/TestVisual.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVisual : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(.5f, .3f, .2f);
+    }
+}
